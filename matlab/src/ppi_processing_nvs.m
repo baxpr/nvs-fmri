ed89ff00db2d62970927081ee4b4a130f8c39536
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2026-06-01 18:26:12 -0500

    ppiwip

--- matlab/src/ppi_processing_nvs.m
+++ matlab/src/ppi_processing_nvs.m
@@ -14,7 +14,7 @@ copyfile(inp.ppiroi_niigz, roi_dir)
 gunzip(fullfile(roi_dir,[n e]))
 ppiroi_nii = fullfile(roi_dir,n);
 V = spm_vol(ppiroi_nii);
-Yroi = spm_readvols(V);
+Yroi = spm_read_vols(V);
 roiinds = unique(Yroi(:));
 roiinds = roiinds(roiinds~=0);
 roilabels = readtable(inp.ppiroilabels_tsv);
