8caad9b2898889bfe43e9fd3e1990c3ef922f107
Baxter Rogers <rogerbp1@cn1260.hw.accre.vu>
2026-06-02 18:57:22 -0500

    compiled

---
+++ matlab/external/spm8_r6313/spm8/config/spm_cfg_static_tools.m
@@ -0,0 +1,3 @@
+function values = spm_cfg_static_tools
+% Static listing of all batch configuration files in the SPM toolbox folder
+values = {spm_cfg_sendmail tbx_cfg_dartel tbx_cfg_fieldmap tbx_cfg_hdw tbx_cfg_render tbx_cfg_preproc8 tbx_cfg_shoot  };
