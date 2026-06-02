9a72184309820f1cba150a47bdde38e180202e0a
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2026-06-02 12:53:52 -0500

    compiler

--- matlab/spm_make_standalone_local.m
+++ matlab/spm_make_standalone_local.m
@@ -1,34 +1,27 @@
-function spm_make_standalone_local(spmdir,outdir,extradir)
-% Compile SPM as a standalone executable using the MATLAB Compiler
-%   https://www.mathworks.com/products/compiler.html
+function spm_make_standalone_local(spmdir,outdir,extradir1,extradir2,extradir3)
+% Compile SPM as a standalone executable using the MATLAB compiler
+%   http://www.mathworks.com/products/compiler/
 %
-% This will generate a standalone application, which can be run outside
-% MATLAB, and therefore does not require a MATLAB licence.
+% This will generate a standalone program, which can be run
+% outside MATLAB, and therefore does not use up a MATLAB licence.
 %
 % On Windows:
-%   spm12.exe <modality>
-%   spm12.exe batch <batch.m(at)>
+%   spm8_wxx.exe <modality>
+%   spm8_wxx.exe run <batch.m(at)>
 %
 % On Linux/Mac:
-%   ./run_spm12.sh <MCRroot> <modality>
-%   ./run_spm12.sh <MCRroot> batch <batch.m(at)>
+%   ./run_spm8.sh <MCRroot> <modality>
+%   ./run_spm8.sh <MCRroot> run <batch.m(at)>
 %
 % The first command starts SPM in interactive mode with GUI. The second
-% executes a batch file or starts the Batch Editor if none is provided.
+% executes a batch file or starts the Batch Editor if empty.
 %
-% Full list of options is accessible from:
-%   ./run_spm12.sh <MCRroot> --help
-%
-% When deployed, compiled applications will require the MATLAB Runtime:
-%   https://www.mathworks.com/products/compiler/matlab-runtime.html
-% 
-% See spm_standalone.m and https://en.wikibooks.org/wiki/SPM/Standalone
+% See spm_standalone.m
 %__________________________________________________________________________
-% Copyright (C) 2010-2019 Wellcome Trust Centre for Neuroimaging
+% Copyright (C) 2010-2011 Wellcome Trust Centre for Neuroimaging
 
 % Guillaume Flandin
-% $Id: spm_make_standalone.m 7534 2019-02-20 17:09:45Z guillaume $
-
+% $Id: spm_make_standalone.m 4385 2011-07-08 16:53:38Z guillaume $
 
 % Updated to add an extra dir to the compile path. Also, we need SPM in the
 % path, and also need to trigger it's auto-add of config paths
@@ -38,9 +31,9 @@ if ~exist(outdir,'dir'), mkdir(outdir); end
 gateway = 'spm_standalone.m';
 contentsver = '';
 
-
-%-Check startup.m
+%-Care of startup.m
 %--------------------------------------------------------------------------
+% see http://www.mathworks.com/support/solutions/data/1-QXFMQ.html?1-QXFMQ
 if exist('startup','file')
     warning('A startup.m has been detected in %s.\n',...
         fileparts(which('startup')));
@@ -50,29 +43,53 @@ end
 %==========================================================================
 %-Static listing of SPM toolboxes
 %==========================================================================
-fid = fopen(fullfile(spm('Dir'),'config','spm_cfg_static_tools.m'),'wt');
+fid = fopen(fullfile(spm('dir'),'config','spm_cfg_static_tools.m'),'wt');
 fprintf(fid,'function values = spm_cfg_static_tools\n');
 fprintf(fid,...
     '%% Static listing of all batch configuration files in the SPM toolbox folder\n');
+% create code to insert toolbox config
+%-Toolbox autodetection
 %-Get the list of toolbox directories
 tbxdir = fullfile(spm('Dir'),'toolbox');
-d = [tbxdir; cellstr(spm_select('FPList',tbxdir,'dir'))];
+d  = dir(tbxdir); d = {d([d.isdir]).name};
+dd = regexp(d,'^\.');
+%(Beware, regexp returns an array if input cell array is of dim 0 or 1)
+if ~iscell(dd), dd = {dd}; end
+d  = {'' d{cellfun('isempty',dd)}};
 ft = {};
-%-Look for '*_cfg_*.m' files in these directories
-for i=1:numel(d)
-    fi = spm_select('List',d{i},'.*_cfg_.*\.m$');
+ftc = {};
+%-Look for '*_cfg_*.m' or '*_config_*.m' files in these directories
+for i=1:length(d)
+    d2 = fullfile(tbxdir,d{i});
+    di = dir(d2); di = {di(~[di.isdir]).name};
+    f2 = regexp(di,'.*_cfg_.*\.m$');
+    if ~iscell(f2), f2 = {f2}; end
+    fi = {di{~cellfun('isempty',f2)}};
     if ~isempty(fi)
-        ft = [ft(:); cellstr(fi)];
+        ft = [ft(:); fi(:)];
+    else
+        % try *_config_*.m files, if toolbox does not have '*_cfg_*.m' files
+        f2 = regexp(di,'.*_config_.*\.m$');
+        if ~iscell(f2), f2 = {f2}; end
+        fi = {di{~cellfun('isempty',f2)}};
+        ftc = [ftc(:); fi(:)];
+    end;
+end
+if ~isempty(ft)||~isempty(ftc)
+    if isempty(ft)
+        ftstr = '';
+    else
+        ft = cellfun(@(cft)strtok(cft,'.'),ft,'UniformOutput',false);
+        ftstr  = sprintf('%s ', ft{:});
     end
+    if isempty(ftc)
+        ftcstr = '';
+    else
+        ftc = cellfun(@(cftc)strtok(cftc,'.'),ftc,'UniformOutput',false);
+        ftcstr = sprintf('cfg_struct2cfg(%s) ', ftc{:});
+    end
+    fprintf(fid,'values = {%s %s};\n', ftstr, ftcstr);
 end
-%-Create code to insert toolbox config
-if isempty(ft)
-    ftstr = '';
-else
-    ft = spm_file(ft,'basename');
-    ftstr = sprintf('%s ', ft{:});
-end
-fprintf(fid,'values = {%s};\n', ftstr);
 fclose(fid);
 
 %==========================================================================
@@ -86,48 +103,16 @@ cfg_util('dumpcfg');
 sts = copyfile(fullfile(spm('Dir'),'Contents.m'),...
                fullfile(spm('Dir'),'Contents.txt'));
 if ~sts, warning('Copy of Contents.m failed.'); end
-if ~isempty(contentsver)
-    % Format: 'xxxx (SPMx) dd-mmm-yyyy'
-    f = fileread(fullfile(spm('Dir'),'Contents.txt'));
-    f = regexprep(f,'% Version \S+ \S+ \S+',['% Version ' contentsver]);
-    fid = fopen(fullfile(spm('Dir'),'Contents.txt'),'w');
-    fprintf(fid,'%s',f);
-    fclose(fid);
-end
-
-%==========================================================================
-%-Trim FieldTrip
-%==========================================================================
-d = fullfile(spm('Dir'),'external','fieldtrip','compat');
-d = cellstr(spm_select('FPList',d,'dir'));
-for i=1:numel(d)
-    f = spm_file(d{i},'basename');
-    nrmv = strncmp(f,'matlablt',8);
-    if nrmv
-        [dummy,I] = sort({f(9:end),version('-release')});
-        nrmv = I(1) == 2;
-    end
-    if ~nrmv
-        [sts, msg] = rmdir(d{i},'s');
-    end
-end
 
 %==========================================================================
 %-Compilation
 %==========================================================================
-Ropts = {'-R','-singleCompThread'} ;
-if ~ismac && spm_check_version('matlab','8.4') >= 0
-    Ropts = [Ropts, {'-R','-softwareopengl'}];
-end
-license('checkout','statistics_toolbox')
-license('checkout','signal_toolbox')
-mcc('-m', '-C', '-v',...
-    '-o',lower(spm('Ver')),...
+mcc('-m', '-C', '-v', '-o',lower(spm('Ver')), 'spm_standalone.m',...
     '-d',outdir,...
-    '-N',...
-    '-p',fullfile(matlabroot,'toolbox','signal'), ...
-    '-p',fullfile(matlabroot,'toolbox','stats'), ...
-    Ropts{:},...
+    '-N','-p',fullfile(matlabroot,'toolbox','signal'),...
+    '-R','-singleCompThread',...
     '-a',spm('Dir'),...
-    '-a',extradir,...
-    gateway);
+    '-a',extradir1,...
+    '-a',extradir2,...
+    '-a',extradir3...
+    )
